7a4b3606981d190ad4b82df12342266bd6bca371
Ditrex24 <vasquezsergiodg@gmail.com>
2023-08-19 19:40:50 -0400

    Nuevo

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+const { loadBD } = require ("../server/loadBD.js")
+const server = require("./src/server");
+const { conn } = require('./src/db.js');
+const PORT = 3001;
+
+conn.sync({ force: true }).then(() => {
+server.listen(PORT, () => {
+  console.log(`Importando info a la BD`);
+  loadBD()
+  console.log(`Server listening on port ${PORT}`);
+})
+}).catch(error => console.error(error))
+  
---
+++ server/src/models/Country.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("Country", {
+    countryid: {
+      type: DataTypes.STRING,
+      defaultValue: DataTypes.UUIDV4,
+     allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      defaultValue: " ",
+    },
+    subregion: {
+      type: DataTypes.STRING,
+    },
+    area: {
+      type: DataTypes.FLOAT,
+    },
+    population: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },{
+    timestamps: false,
+  });
+};
